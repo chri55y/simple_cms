d81c3caaea074d60cb725e63700133d42fadc7fe
ChrissyHormes <Christine.Hormes@gmail.com>
2017-09-18 17:04:25 -0500

    chapter 4.9 challenge: my attempt, only 2 FKs

--- db/migrate/20170918203727_create_sections.rb
+++ db/migrate/20170918203727_create_sections.rb
@@ -1,8 +1,16 @@
 class CreateSections < ActiveRecord::Migration[5.1]
-  def change
+  def up
     create_table :sections do |t|
-
+      t.integer "page_id"
+      t.string "name"
+      t.integer "position"
+      t.boolean "visible", :default => false
       t.timestamps
     end
+    add_index("sections", "page_id")
+  end
+
+  def down
+    drop_table :sections
   end
 end
