734621dec6c7d51cc71effb561bc696a4a00dfeb
ChrissyHormes <Christine.Hormes@gmail.com>
2017-09-28 17:50:45 -0500

    ch 5.11.1: initial named scope additions to Subject Class

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,7 @@
 class Subject < ApplicationRecord
+
+  scope :visible, lambda { where(:visible => true) }
+  scope :invisible, lambda { where(:visible => false) }
+  scope :sorted, lambda { order("position ASC") }
+
 end
