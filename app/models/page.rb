a2cf771ace534aaadf15be1aa0642e30a901ab82
ChrissyHormes <Christine.Hormes@gmail.com>
2017-09-28 19:48:01 -0500

    ch 6.2: implement 1:1 relationship, as an exercise, not permanent

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,5 @@
 class Page < ApplicationRecord
+
+  belongs_to :subject, optional: true # foreign key info would go here, if we didn't already have a FK on pages
+      # had to add the ", optional: true" above to get around Rails validations
 end
