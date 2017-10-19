fb9ab097c7138579ab4e488946042253e144c200
chri55y <11790059+chri55y@users.noreply.github.com>
2017-10-18 20:56:58 -0500

    ch 7.1: update routes before learning REST approach

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
 
   get 'subjects/index'
-
   get 'subjects/show'
-
   get 'subjects/new'
-
   get 'subjects/edit'
-
   get 'subjects/delete'
 
   root 'demo#index'
