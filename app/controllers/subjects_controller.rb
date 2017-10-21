cf2a027c34467bb8f7abf8db40ab7c94111faf14
chri55y <11790059+chri55y@users.noreply.github.com>
2017-10-20 20:24:51 -0500

    ch 8.1: define index action in SubjectsController

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,5 +1,9 @@
 class SubjectsController < ApplicationController
+
   def index
+    # find all subjects so we can list them
+    @subjects = Subject.sorted # generic form: Subject.all, but a sorted list is better
+    # render('index') # this is done by default, so we don't need to include it
   end
 
   def show
