b2e2d1da80ad1ee5c532a1a50f0d5f720776f949
chri55y <11790059+chri55y@users.noreply.github.com>
2017-11-08 12:29:44 -0600

    ch 8 challenge: generate Pages controller and 5 CRUD view templates

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
 
+  get 'pages/index'
+
+  get 'pages/show'
+
+  get 'pages/new'
+
+  get 'pages/edit'
+
+  get 'pages/delete'
+
   root 'demo#index'
 
   resources :subjects do
