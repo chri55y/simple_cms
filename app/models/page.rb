476fe8e3c541eb58324a9cd6fe114e0bf9c08c36
chri55y <11790059+chri55y@users.noreply.github.com>
2017-11-08 13:10:13 -0600

    ch 8 challenge: add :position_sorted scope to Page model

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,4 +3,10 @@ class Page < ApplicationRecord
   belongs_to :subject, {:optional => true}
   has_many :sections
   has_and_belongs_to_many :admin_users
+
+  # add scopes
+  # (videos do not include this step for Page)
+  scope :position_sorted , lambda {order("position ASC")}
+
+
 end
