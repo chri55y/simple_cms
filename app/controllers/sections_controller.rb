c5cbb10cb9407ac5decee8effd62766b68ab7d67
chri55y <11790059+chri55y@users.noreply.github.com>
2017-11-08 19:37:12 -0600

    ch 8 challenge: SectionsController: add strong params and (empty) method defs for remaining CRUD actions

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -8,9 +8,27 @@ class SectionsController < ApplicationController
   def new
   end
 
+  def create
+  end
+
   def edit
+  end
+
+  def update
   end
 
   def delete
   end
+
+  def destroy
+  end
+
+  private
+
+  def sections_strong_params
+    params.require(:section).permit(:name, :position, :visible, :page_id)
+  end
+
+end
+
 end
