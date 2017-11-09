6f32d144b48cae4252236ab2cfa13f374802015f
chri55y <11790059+chri55y@users.noreply.github.com>
2017-11-08 20:27:04 -0600

    ch 8 challenge SECTION: CREATE method in Controller

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -12,6 +12,13 @@ class SectionsController < ApplicationController
   end
 
   def create
+    @section = Section.new(section_strong_params)
+    if @section.save
+      flash[:notice] = "Successfully created Section"
+      redirect_to(sections_path)
+    else
+      render('new')
+    end
   end
 
   def edit
@@ -30,10 +37,8 @@ class SectionsController < ApplicationController
 
   private
 
-  def sections_strong_params
+  def section_strong_params
     params.require(:section).permit(:name, :position, :visible, :page_id)
   end
-
-end
 
 end
