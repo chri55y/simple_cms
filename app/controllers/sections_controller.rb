3626ab41ba7c7a1e5e3bd8541b8de7f53456123e
chri55y <11790059+chri55y@users.noreply.github.com>
2017-11-08 20:12:01 -0600

    ch 8 challenge SECTION: complete single line method definitions in controller (index, show, edit, delete, new)

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,23 +1,28 @@
 class SectionsController < ApplicationController
   def index
+    @sections = Section.position_sorted
   end
 
   def show
+    @section = Section.find(params[:id])
   end
 
   def new
+    @section = Section.new
   end
 
   def create
   end
 
   def edit
+    @section = Section.find(params[:id])
   end
 
   def update
   end
 
   def delete
+    @section = Section.find(params[:id])
   end
 
   def destroy
